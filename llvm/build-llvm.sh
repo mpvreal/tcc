cd7d946a9e497a86df4109532480187b8914d4a9
Matheus Pires Vila Real <matheus.pires.vila@uel.br>
2024-02-21 18:47:19 -0300

    Script de build do LLVM

---
+++ llvm/build-llvm.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+BUILD_LLVM=`pwd`/build
+INSTALL_LLVM=`pwd`/install
+LLVM_SRC=`pwd`/llvm-project
+
+#rm -rf $BUILD_LLVM
+#rm -rf $INSTALL_LLVM
+
+mkdir -p $BUILD_LLVM
+mkdir -p $INSTALL_LLVM
+
+#cd $BUILD_LLVM
+
+cmake -G Ninja \
+	  -S $LLVM_SRC/llvm \
+	  -B $BUILD_LLVM \
+	  -DCMAKE_INSTALL_PREFIX=$INSTALL_LLVM \
+	  -DCMAKE_BUILD_TYPE=debug \
+	  -DLLVM_OPTIMIZED_TABLEGEN=ON \
+	  -DCMAKE_CXX_STANDARD=17 \
+	  -DCMAKE_C_COMPILER=$(which clang) \
+	  -DCMAKE_CXX_COMPILER=$(which clang++) \
+	  -DLLVM_ENABLE_LLD=ON \
+	  -DCMAKE_CXX_LINK_FLAGS="-Wl,-rpath,/usr/lib" \
+	  -DLLVM_PARALLEL_COMPILE_JOBS=4 \
+	  -DLLVM_PARALLEL_LINK_JOBS=1 \
+	  -DLLVM_TARGETS_TO_BUILD="X86;AArch64;ARM" \
+	  -DLLVM_LIT_ARGS=-v
+	  
+ninja -C $BUILD_LLVM llc
